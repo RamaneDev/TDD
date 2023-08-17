1c6b2d5e3980a24a4ef704b0641e021a0744bcdd
mesri <abde_mes@yahoo.fr>
2023-08-17 14:26:03 +0200

    Get_OnSuccess_ReurnsStatusCode200

---
+++ Customers.API/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Customers.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+            
+        }
+
+
+        [HttpGet(Name = "GetUsers")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("all good");
+        }
+    }
+}
---
+++ Customersapi.Tests/Controllers/TestUsersController.cs
@@ -0,0 +1,25 @@
+﻿using Customers.API.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Customersapi.Tests.Controllers
+{
+    public class TestUsersController
+    {
+        [Fact]
+        public async Task Get_OnSuccess_ReurnsStatusCode200()
+        {
+            //Arrange
+            var sut = new UsersController();
+
+            //Act
+            var result = (OkObjectResult)await sut.Get();
+
+            //Assert
+            result.StatusCode.Should().Be(200);
+
+        }
+    }
+}
